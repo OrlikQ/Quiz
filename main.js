e37b2389114dd4d912cbfe3c60c3222e6a6fe852
OrlikQ <norbertkielbasa5@gmail.com>
2022-10-19 15:26:21 +0200

    quiz

--- main.js
+++ main.js
@@ -1,16 +1,64 @@
-const form = document.querySelector(`.quiz-box`)
-const answers = document.querySelectorAll(`.answer`)
-
-const modal = document.querySelector(`.modal`)
-const modalInfo = modal.querySelector(`p`)
-const modalBtn = modal.querySelector(`.close-modal`)
+const form = document.querySelector('.quiz-box')
+const answers = Array.from(document.querySelectorAll('.answer'))
+const allQuestions = document.querySelectorAll('question')
+const modal = document.querySelector('.modal')
+const modalInfo = modal.querySelector('p')
+const modalBtn = modal.querySelector('.close-modal')
 
 const handleQuiz = e => {
     e.preventDefault();
    
     const checkedAnswers = answers.filter(answer => answer.checked)
-    console.log(checkedAnswers);
+    const isTrue = checkedAnswers.every(answer => answer.value === 'true')
+    const allChecked = checkedAnswers.lenght === allQuestions.length 
+
+    if(!allChecked) {
+        modal.classList.add('modal-active')
+        modalInfo.textContent = 'Wybierz wszytskie odpowiedzi!!!'
+    }
+
+    checkedAnswers.forEach(answer => {
+        const checkIfcorrect = answer.value === 'true'
+        const answerBox = answer.closest('.answer-box')
+
+        if(checkIfcorrect) {
+            answerBox.classList.add('correct')
+            answerBox.classList.remove('incorrect')
+        } else {
+            answerBox.classList.add('incorrect')
+            answerBox.classList.remove('correct')
+        }
+    })
+
+    if (isTrue && allChecked) {
+        modal.classList.add('modal-active')
+        modalInfo.textContent = 'Brawo wszystkie odpowiedzi są poprawne😄'
+    } else {
+    modal.classList.add('modal-active')
+    modalInfo.textContent = 'Przykro mi przegrałeś😢'
+    }
 }
+
+const closeModal = () => {
+    modal.classList.remove('modal-active')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+modalBtn.addEventListener('click', closeModal)
+form.addEventListener('submit', handleQuiz)
 
 
 
